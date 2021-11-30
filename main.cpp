3047a7f7cd218133e1ca1b06404d5aadd1718cf8
Rosemary Koshy <rosemary.koshy@reed.edu>
2021-11-29 19:53:03 -0800

    test

--- main.cpp
+++ main.cpp
@@ -5,11 +5,25 @@
  * C++17
  * A self-contained RISC-V assembler and simulator that reads and executes
  * RV32I Base Integer Instruction Set assembly programs.
+ * fetch, decode, execute, memory, write-back
  */
 #include <iostream>
 #include <cstdlib>
+#include <fstream>
+#include <string>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+int main(int argc, char* argv[]) {
+    std::ifstream file;
+    std::string line;
+    file.open("input.txt");
+    if (!file) {
+        perror("Cannot open file");
+        return EXIT_FAILURE;
+    }
+    while (file) {
+        file >> line;
+        std::cout << line << '\n';
+    }
+    file.close();
     return EXIT_SUCCESS;
 }
