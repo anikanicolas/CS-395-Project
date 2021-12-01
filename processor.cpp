ce85cde418ebee44efa82c16d176a7a1cbc6265e
anikanicolas <iamanikonda@gmail.com>
2021-12-01 09:32:41 -0800

    changed decode more

--- processor.cpp
+++ processor.cpp
@@ -14,15 +14,24 @@ std::string xfind(const std::map<std::string, std::string> &map,
     if (it == map.end()) return "FAILURE";
     return it->second;
 }
-
+//convert binary string to a decimal number
 size_t to_decimal(const std::string &binary_string) {
-    return stoul(binary_string, nullptr, 2);
+    unsigned long num = stoul(binary_string, nullptr, 2);
+    int decimal = 0;
+    int count = 0;
+    int remainder = 0;
+    while(num != 0) {
+      remainder = num % 10;
+      num /= 10;
+      decimal += remainder * pow(2, count);
+      ++count;
+    }
+    return decimal;
 }
 
-// decode stage
-std::string decode(std::string inst) {
-    // set inst
+// set inst map getters
 
+std::map<std::string, std::string> get_op_map() {
     std::map<std::string, std::string> op_map;
     op_map["010000"] = "operation";
     op_map["010001"] = "comparison";
@@ -33,22 +42,38 @@ std::string decode(std::string inst) {
     op_map["011010"] = "jump";
     op_map["000100"] = "op on line 1 of input.txt";
     op_map["000101"] = "op on line 2 of input.txt";
+    return op_map;
+}
 
+std::map<std::string, std::string> get_func_map() {
     std::map<std::string, std::string> func_map;
     func_map["0100000"] = "addq";
     func_map["0101001"] = "subq";
+    return func_map;
+}
 
-    std::map<std::string, std::string> funcCodesComparison;
-    funcCodesComparison["0100000"] = "or/bis";
-    funcCodesComparison["1000000"] = "xor";
-    funcCodesComparison["0100100"] = "cmoveq";
-    funcCodesComparison["1001101"] = "cmplt";
+std::map<std::string, std::string> get_comp_map() {
+    std::map<std::string, std::string> comp_map;
+    comp_map["0100000"] = "or/bis";
+    comp_map["1000000"] = "xor";
+    comp_map["0100100"] = "cmoveq";
+    comp_map["1001101"] = "cmplt";
+    return comp_map;
+}
 
-    std::map<std::string, std::string> hintCodes;
-    hintCodes["00"] = "jmp";
-    hintCodes["01"] = "jsr";
-    hintCodes["10"] = "ret";
+std::map<std::string, std::string> get_jump_map() {
+    std::map<std::string, std::string> jump_map;
+    jump_map["00"] = "jmp";
+    jump_map["01"] = "jsr";
+    jump_map["10"] = "ret";
+    return jump_map;
+}
 
+// decode stage
+std::string decode(std::string inst, const std::map<std::string, std::string> &op_map,
+                   const std::map<std::string, std::string> &func_map,
+                   const std::map<std::string, std::string> &comp_map,
+                   const std::map<std::string, std::string> &jump_map) {
     std::string op = xfind(op_map, inst.substr(0, 6));
     if (op == "operation") {
         std::string func = xfind(func_map, inst.substr(20, 27));
@@ -60,6 +85,15 @@ std::string decode(std::string inst) {
                 return op + ' ' + func + ' ' + std::to_string(ra) + ' ' +
                        std::to_string(rb);
             }
+            else if (inst[19] == 1) {
+              // if the bit 12 is 1, then it's an RI type inst
+              
+            }
+        }
+        else if (func == "subq") {
+          if (inst[19] == 0) {
+
+          }
         }
         return op + ' ' + func;
     }
