7f398a6943f2f15481e6dffd6dfdf75ccefd9b85
Rosemary Koshy <rosemary.koshy@reed.edu>
2021-12-05 22:20:36 -0800

    due

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 /**
  * Computer Architecture Project
  * Anika Nicolas & Rosemary Koshy
- * Due: Friday, November 12 at 1:30 PM
+ * Due: Wed, Dec 8 at 1:30 PM
  * C++17
  * A RISC-V processor simulator that reads and executes binary programs.
  * https://github.com/riscv/riscv-isa-manual/releases/download/Ratified-IMAFDQC/riscv-spec-20191213.pdf
@@ -9,12 +9,11 @@
  * https://en.wikipedia.org/wiki/Instruction_pipelining
  * https://en.wikipedia.org/wiki/Classic_RISC_pipeline
  * 1. Instruction fetch: fetch instructions from memory
- * 2. Instruction decode and register fetch: read registers and decode the
- * instruction
+ * 2. Instruction decode and register fetch: read registers and decode instruction
  * 3. Execute: execute the instruction or calculate an address
  * 4. Memory access: access an operand in data memory
  * 5. Register write back: write the result into a register
- * TODO: ALU, decode, ...
+ * TODO: ALU, ...
  * TODO(anikanicolas): add more instructions to input.txt or add input1.txt
  */
 #include <cstdint>
