b2292383b627f27f25b8abfe3074634e95c9e0d7
Rosemary Koshy <rosemary.koshy@reed.edu>
2021-12-06 18:00:43 -0800

    try to simplify iexecute.cpp

--- iexecute.cpp
+++ iexecute.cpp
@@ -5,16 +5,42 @@
 #include <vector>
 
 // execute stage [[ unused ]]
+/**
+ * Execute decoded instruction with fetched registers
+ * @param decoded instruction to execute
+ * @return executed instruction
+ */
 std::vector<std::string> iexecute(const std::vector<std::string> &decoded) {
-    std::vector<std::string> ret;
-    if (decoded[0] == "ADD") {
-        // ADD performs the addition of rs1 and rs2
-        ret = {"WB", decoded[1],
-               std::to_string(stoul(decoded[2]) + stoul(decoded[3]))};
-    } else if (decoded[0] == "SUB") {
-        // SUB performs the subtraction of rs2 from rs1
-        ret = {"WB", decoded[1],
-               std::to_string(stoul(decoded[2]) - stoul(decoded[3]))};
+    /// execution state to return, FAILURE by default
+    std::vector<std::string> ret = {"FAILURE"};
+    if (!decoded.empty()) {
+        std::string op = decoded[0];
+        std::string dest;
+        std::string src1;
+        std::string src2;
+        if (decoded.size() > 1) {
+            dest = decoded[1];
+        }
+        if (decoded.size() > 2) {
+            src1 = decoded[2];
+        }
+        if (decoded.size() > 3) {
+            src2 = decoded[3];
+        }
+        if (op == "LUI") {
+            /*
+             * A LUI instruction can first load src1 with the upper 20 bits of a target address
+             * LUI (load upper immediate) places the 20-bit U-immediate into bits 31–12 of register dest
+             * and places zero in the lowest 12 bits.
+             */
+            ret = {"WB", dest, "0b"};
+        } else if (op == "ADD") {
+            // ADD performs the addition of src1 and src2
+            ret = {"WB", dest, std::to_string(stoul(src1) + stoul(src2))};
+        } else if (op == "SUB") {
+            // SUB performs the subtraction of src2 from src1
+            ret = {"WB", dest, std::to_string(stoul(src1) - stoul(src2))};
+        }
     }
     return ret;
 }
